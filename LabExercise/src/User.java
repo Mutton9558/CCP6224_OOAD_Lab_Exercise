f279f4da4731c8d5989d201801d757b772af89d8
JeiKarousel <theconfid3ntialz@gmail.com>
2026-05-25 12:06:08 +0800

    just edited stuff

--- LabExercise/src/User.java
+++ LabExercise/src/User.java
@@ -1,3 +1,30 @@
 public class User {
+
+    private int user_ID, user_Age;
+    private String user_Name, password, user_Gender;
+
+    public User(String username, String password){
+
+        user_Name = username;
+        this.password = password;
+    }
+
+    //Getters
+    public int getUserID(){
+        return user_ID;
+    }
+
+    public String getUserName(){
+        return user_Name;
+    }
+
+    public int getUserAge(){
+        return user_Age;
+    }
     
+    public String getUserGender(){
+        return user_Gender;
+    }
+
+    //do we need getUserPassword?
 }
