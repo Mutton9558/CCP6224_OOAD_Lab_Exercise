9cec43e95e845edd59dbd84d35bee992a244c23c
JeiKarousel <theconfid3ntialz@gmail.com>
2026-05-25 10:35:55 +0800

    just added empty classes

---
+++ LabExercise/src/Diagnosis.java
@@ -0,0 +1,9 @@
+import java.time.LocalDate;
+
+public class Diagnosis {
+
+    private String diagnosis_Name;
+    private int diagnosis_ID;
+    private LocalDate diagnosis_Date;
+    
+}
--- LabExercise/src/Prescription.java
+++ LabExercise/src/Prescription.java
@@ -1,7 +1,9 @@
+import java.time.LocalDate;
 
 public class Prescription{
 
-    private String prescription_Name, prescription_Condition;
+    private String prescription_Name, prescription_Dose, prescription_Condition, prescription_Frequency;
+    private LocalDate prescription_Date;
     private int prescription_ID;
     
 }
