bf554d5f90bc9ee704090322f5e0267f23669d07
OrpheusJaso <jasonwan9937@gmail.com>
2026-05-25 22:30:05 +0800

    Added more to DoctorController

--- LabExercise/src/Doctor.java
+++ LabExercise/src/Doctor.java
@@ -1,14 +1,16 @@
+import java.util.*;
+
 public class Doctor extends User{
     protected int doctor_ID;
-    protected String location;
-    protected Patient[] PatientList;
+    protected String office;
+    protected List<Patient> patientList = new ArrayList<>();
     protected String specialisation;
     protected Appointment[] AppointmentList;
 
-    public Doctor(int userID, String username, String pass, String location, String specialisation){
+    public Doctor(int userID, String username, String pass, String office, String specialisation){
         super(username, pass);
         this.doctor_ID = userID;
-        this.location = location;
+        this.office = office;
         this.specialisation = specialisation;
     }
 }
